ec3e4a85763c277e3f4ce194a95be3b26d0f1f3f
flores-97 <floresvictor15@gmail.com>
2025-04-22 09:34:52 -0700

    created CleaningBrands and CleaningInventory

---
+++ COMP003B.Assignment5/Data/CleaningInventory.cs
@@ -0,0 +1,14 @@
+﻿using COMP003B.Assignment5.Models;
+namespace COMP003B.Assignment5.Data
+{
+    public class CleaningInventory
+    {
+        public static List<CleaningBrand> CleaningBrands { get; } = new()
+        {
+            new CleaningBrand { Id =1, Name = "Clorox", Quantity = 3, Price = 12.99m },
+            new CleaningBrand { Id =1, Name = "Windex", Quantity = 3, Price = 12.99m },
+            new CleaningBrand { Id =1, Name = "Ajax", Quantity = 3, Price = 12.99m },
+            new CleaningBrand { Id =1, Name = "Fabuloso", Quantity = 3, Price = 12.99m },
+        };
+    }
+}
---
+++ COMP003B.Assignment5/Models/CleaningBrand.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP003B.Assignment5.Models
+{
+    public class CleaningBrand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
