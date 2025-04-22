55a23810a25ca18918195677117f1233fe8dc9c5
flores-97 <floresvictor15@gmail.com>
2025-04-22 15:00:14 -0700

    all entries work correctly.

--- COMP003B.Assignment5/Data/CleaningInventory.cs
+++ COMP003B.Assignment5/Data/CleaningInventory.cs
@@ -6,9 +6,9 @@ namespace COMP003B.Assignment5.Data
         public static List<CleaningBrand> CleaningBrands { get; } = new()
         {
             new CleaningBrand { Id =1, Name = "Clorox", Quantity = 3, Price = 12.99m },
-            new CleaningBrand { Id =1, Name = "Windex", Quantity = 1, Price = 2.99m },
-            new CleaningBrand { Id =1, Name = "Ajax", Quantity = 4, Price = 5.99m },
-            new CleaningBrand { Id =1, Name = "Fabuloso", Quantity = 7, Price = 15.99m },
+            new CleaningBrand { Id =2, Name = "Windex", Quantity = 1, Price = 2.99m },
+            new CleaningBrand { Id =3, Name = "Ajax", Quantity = 4, Price = 5.99m },
+            new CleaningBrand { Id =4, Name = "Fabuloso", Quantity = 7, Price = 15.99m },
         };
     }
 }
